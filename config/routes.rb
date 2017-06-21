5fa6845e6adb0440b29491674b1703505d594d7c
Julio Berina <julioberina@gmail.com>
2017-06-21 20:49:31 +0000

    Implemented edit and update actions with succeeding tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :grams, only: [:new, :create, :show]
+  resources :grams, except: [:index, :destroy]
   root to: "grams#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#update action" do
+    it "should allow users to successfully update grams" do
+      gram = FactoryGirl.create(:gram, message: "Initial value")
+      patch :update, params: { id: gram.id, gram: { message: "Changed" } }
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq 'Changed'
+    end
+
+    it "should have a 404 error if the gram cannot be found" do
+      patch :update, params: { id: "YOLOSWAG", gram: { message: "Changed" } }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessable entity" do
+      gram = FactoryGirl.create(:gram, message: "Initial value")
+      patch :update, params: { id: gram.id, gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "Initial value"
+    end
+  end
+
+  describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should returna 404 error if the gram is not found" do
+      get :edit, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#show action" do
     it "should successfully show the page if the gram is found" do
       gram = FactoryGirl.create(:gram)
